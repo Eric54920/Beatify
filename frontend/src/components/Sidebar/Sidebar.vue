419a9c6cecace4c1c08b929a764b3f49c5ddc5cb
Eric54920 <realmaguodong@outlook.com>
2024-10-01 01:08:52 +0800

    feat:添加主页

---
+++ frontend/src/components/Sidebar/Sidebar.vue
@@ -0,0 +1,167 @@
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Search } from 'lucide-vue-next'
+import { Input } from '@/components/ui/input'
+import {
+  ContextMenu,
+  ContextMenuCheckboxItem,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuLabel,
+  ContextMenuRadioGroup,
+  ContextMenuRadioItem,
+  ContextMenuSeparator,
+  ContextMenuShortcut,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from '@/components/ui/context-menu'
+
+export default defineComponent({
+  components: {
+    Button,
+    ScrollArea,
+    Search,
+    Input,
+    ContextMenu,
+    ContextMenuCheckboxItem,
+    ContextMenuContent,
+    ContextMenuItem,
+    ContextMenuLabel,
+    ContextMenuRadioGroup,
+    ContextMenuRadioItem,
+    ContextMenuSeparator,
+    ContextMenuShortcut,
+    ContextMenuSub,
+    ContextMenuSubContent,
+    ContextMenuSubTrigger,
+    ContextMenuTrigger
+  },
+  data() {
+    return {
+      playlists: [
+        'Recently Added',
+      ]
+    }
+  },
+  methods: {
+    cn
+  },
+  props: {
+
+  }
+})
+</script>
+
+<template>
+  <div :class="cn('pb-12', $attrs.class ?? '')">
+    <div class="space-y-4 py-4">
+      <div class="px-3 py-2">
+        <div class="relative w-full mb-2 max-w-sm items-center">
+          <Input id="search" type="text" placeholder="Search..." class="pl-10" />
+          <span class="absolute start-0 inset-y-0 flex items-center justify-center px-2">
+            <Search class="size-6 text-muted-foreground" />
+          </span>
+        </div>
+      </div>
+      <div class="px-3 py-2">
+        <h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">
+          Discover
+        </h2>
+        <div class="space-y-1">
+          <Button variant="ghost" class="w-full justify-start">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="mr-2 h-4 w-4">
+              <rect width="7" height="7" x="3" y="3" rx="1" />
+              <rect width="7" height="7" x="14" y="3" rx="1" />
+              <rect width="7" height="7" x="14" y="14" rx="1" />
+              <rect width="7" height="7" x="3" y="14" rx="1" />
+            </svg>
+            Browse
+          </Button>
+        </div>
+      </div>
+      <div class="px-3 py-2">
+        <h2 class="mb-2 px-4 text-lg font-semibold tracking-tight">
+          Library
+        </h2>
+        <div class="space-y-1">
+          <Button variant="ghost" class="w-full justify-start">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="mr-2 h-4 w-4">
+              <circle cx="8" cy="18" r="4" />
+              <path d="M12 18V2l7 4" />
+            </svg>
+            Songs
+          </Button>
+          <Button variant="ghost" class="w-full justify-start">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="mr-2 h-4 w-4">
+              <path d="m12 8-9.04 9.06a2.82 2.82 0 1 0 3.98 3.98L16 12" />
+              <circle cx="17" cy="7" r="5" />
+            </svg>
+            Artists
+          </Button>
+          <Button variant="ghost" class="w-full justify-start">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+              strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="mr-2 h-4 w-4">
+              <path d="m16 6 4 14" />
+              <path d="M12 6v14" />
+              <path d="M8 8v12" />
+              <path d="M4 4v16" />
+            </svg>
+            Albums
+          </Button>
+        </div>
+      </div>
+      <div class="py-2">
+        <h2 class="relative px-7 text-lg font-semibold tracking-tight">
+          Playlists
+        </h2>
+        <ScrollArea class="h-[200px] px-1">
+          <div class="space-y-1 p-2">
+            <RouterLink v-for="(playlist, i) in playlists" :key="`${playlist}-${i}`" to="/aa">
+              <ContextMenu>
+                <ContextMenuTrigger>
+                  <Button variant="ghost" class="w-full justify-start font-normal">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                      strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="mr-2 h-4 w-4">
+                      <path d="M21 15V6" />
+                      <path d="M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z" />
+                      <path d="M12 12H3" />
+                      <path d="M16 6H3" />
+                      <path d="M12 18H3" />
+                    </svg>
+                    {{ playlist }}
+                  </Button>
+                </ContextMenuTrigger>
+                <ContextMenuContent class="w-40">
+                  <ContextMenuItem inset>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="mr-2 h-4 w-4">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                    </svg>
+                    编辑
+                  </ContextMenuItem>
+                  <ContextMenuItem inset>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                      stroke="currentColor" class="mr-2 h-4 w-4">
+                      <path stroke-linecap="round" stroke-linejoin="round"
+                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                    </svg>
+                    删除
+                  </ContextMenuItem>
+                </ContextMenuContent>
+              </ContextMenu>
+            </RouterLink>
+          </div>
+        </ScrollArea>
+      </div>
+    </div>
+  </div>
+</template>
--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,6 +1,24 @@
-<script lang="ts" setup>
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Sidebar from '@/components/Sidebar/Sidebar.vue'
 
+export default defineComponent({
+    components: {
+        Sidebar
+    },
+    data () {
+        return {
+            
+        }
+    }
+})
 </script>
+
 <template>
-    Main
+    <div class="grid grid-cols-5 h-screen">
+        <Sidebar/>
+        <div class="col-span-4 border-l">
+            <RouterView/>
+        </div>
+    </div>
 </template>
