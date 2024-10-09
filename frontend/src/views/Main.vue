0aa2245ce27581b606b6f61d7733e443c78160a3
Eric54920 <realmaguodong@outlook.com>
2024-10-10 00:16:07 +0800

    refactor:调整首页布局

--- frontend/src/views/Main.vue
+++ frontend/src/views/Main.vue
@@ -1,13 +1,31 @@
 <script lang="ts" setup>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Sidebar from '@/components/Sidebar/Sidebar.vue'
+
+const pageName = ref("Home")
 </script>
 
 <template>
-    <div class="grid grid-cols-5 h-screen">
-        <Sidebar/>
-        <div class="col-span-4 border-l">
-            <RouterView/>
+    <div class="flex flex-col h-screen">
+        <!-- 上面 -->
+        <div class="flex-1 flex overflow-hidden">
+            <!-- 左侧 -->
+            <Sidebar />
+
+            <!-- 右侧 -->
+            <div class="flex-1 flex-col overflow-y-auto bg-white border-l">
+                <div class="bg-white bg-opacity-50 backdrop-blur-lg sticky top-0 py-2 text-center font-semibold text-gray-700 leading-10 text-xl z-10">
+                    {{ pageName }}
+                </div>
+                <div class="overflow-hidden">
+                    <RouterView />
+                </div>
+            </div>
+        </div>
+
+        <!-- 下面 -->
+        <div class="h-24 sticky inset-x-0 bottom-0 border-t bg-slate-600">
+            footer
         </div>
     </div>
 </template>
