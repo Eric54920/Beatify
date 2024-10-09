6654153ca0099dc09c2b9662fc01e7ea43b39537
Eric54920 <realmaguodong@outlook.com>
2024-10-10 00:15:15 +0800

    refactor:调整音乐列表布局

--- frontend/src/views/Songs.vue
+++ frontend/src/views/Songs.vue
@@ -2,17 +2,10 @@
 import { onMounted, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { GetSongs } from '../../wailsjs/go/beatify/App'
-import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from '@/components/ui/table'
 import Toaster from '@/components/ui/toast/Toaster.vue'
 import { toast } from '@/components/ui/toast'
 import { useToast } from '@/components/ui/toast/use-toast'
+import { ScrollArea } from '@/components/ui/scroll-area'
 
 
 const route = useRoute()
@@ -24,12 +17,13 @@ interface Song {
     album: string,
     type: string,
     size: number,
+    time: number,
     update_at: string
 }
 const songs = ref<Song[]>([])
 
 const getSongs = () => {
-    /* 获取所有歌曲 */ 
+    /* 获取所有歌曲 */
     GetSongs(dir.value).then((res: Record<string, any>) => {
         if (res.status == 500) {
             toast({
@@ -40,6 +34,11 @@ const getSongs = () => {
             songs.value = res.data;
         }
     })
+}
+
+const formatTime = (time: number) => {
+    /* 将秒转为分 */
+    return `${time / 60 | 0}:${(time % 60).toString().padStart(2, '0')}`
 }
 
 // 检测路由中参数的变化
@@ -60,32 +59,26 @@ onMounted(() => {
 
 <template>
     <Toaster />
-    <Table>
-        <TableHeader>
-            <TableRow>
-                <TableHead class="w-[100px]">No.</TableHead>
-                <TableHead>title</TableHead>
-                <TableHead>artist</TableHead>
-                <TableHead>album</TableHead>
-                <TableHead>type</TableHead>
-                <TableHead>size</TableHead>
-                <TableHead class="text-right">update at</TableHead>
-            </TableRow>
-        </TableHeader>
-        <TableBody>
-            <TableRow v-for="(song, i) in songs" :key="song.id">
-                <TableCell class="font-medium">
-                    {{ i + 1 }}
-                </TableCell>
-                <TableCell>{{ song.title }}</TableCell>
-                <TableCell>{{ song.artist }}</TableCell>
-                <TableCell>{{ song.album }}</TableCell>
-                <TableCell>{{ song.type }}</TableCell>
-                <TableCell>{{ song.size }}</TableCell>
-                <TableCell class="text-right">
-                    {{ song.update_at }}
-                </TableCell>
-            </TableRow>
-        </TableBody>
-    </Table>
+
+    <ScrollArea class="h-full overflow-y-auto text-sm text-center">
+        <div class="flex flex-row px-2 h-12 items-center text-stone-500 font-semibold border-b">
+            <div class="basis-1/12">No.</div>
+            <div class="basis-3/12 text-left">Title</div>
+            <div class="basis-2/12 text-left">Artist</div>
+            <div class="basis-3/12 text-left">Album</div>
+            <div class="basis-1/12">Type</div>
+            <div class="basis-1/12">Size</div>
+            <div class="basis-1/12">Time</div>
+        </div>
+        <div class="flex flex-row px-2 h-12 items-center border-b last-of-type:border-none hover:bg-stone-100 transition"
+            v-for="(song, i) in songs" :key="song.id">
+            <div class="basis-1/12 text-stone-600">{{ i + 1 }}</div>
+            <div class="basis-3/12 text-left">{{ song.title }}</div>
+            <div class="basis-2/12 text-left text-stone-600">{{ song.artist }}</div>
+            <div class="basis-3/12 text-left text-stone-600">{{ song.album }}</div>
+            <div class="basis-1/12 text-stone-600">{{ song.type }}</div>
+            <div class="basis-1/12 text-stone-600">{{ song.size }} MB</div>
+            <div class="basis-1/12 text-stone-600">{{ formatTime(song.time) }}</div>
+        </div>
+    </ScrollArea>
 </template>
